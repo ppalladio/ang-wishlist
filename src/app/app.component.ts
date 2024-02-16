d2dd597624d612d0078f669ad383f2d236d4f891
ppalladio <nine.slide@gmail.com>
2024-02-16 22:49:05 +0100

    add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { WishItem } from './../shared/models/wishlistItems';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  items = [
+    new WishItem('item1'),
+    new WishItem('item2', true),
+    new WishItem('item3', false),
+  ];
   title = 'new title';
 }
