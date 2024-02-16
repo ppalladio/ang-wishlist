89dcb46ed3759f4e9a4cb3b5f6bdaa08d967db4c
ppalladio <nine.slide@gmail.com>
2024-02-16 23:13:44 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  items = [
+  items: WishItem[] = [
     new WishItem('item1'),
     new WishItem('item2', true),
     new WishItem('item3', false),
